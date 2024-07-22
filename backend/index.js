2576de75bc21b100b8832e0cf2f06ad668616675
bharath <bunnybharath917@gmail.com>
2024-07-22 00:10:56 +0000

    after changing secure to true and sameSite to lax

--- backend/index.js
+++ backend/index.js
@@ -76,8 +76,9 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: false, // Set to true in production for HTTPS
+      secure: true, // Set to true in production for HTTPS
       maxAge: 30 * 24 * 60 * 60 * 1000, // Session valid for 30 days
+      sameSite: "lax",
     },
   })
 );
